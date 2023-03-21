c53e6b97fa2809aa689ebdb2577f9b641f4db9bb
kohei110 <kohei.nishitani@gmail.com>
2023-03-20 21:12:16 -0700

    update todo app - learning flask

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # https://tech-diary.net/flask-introduction/
 
-from datetime import datetime
-from flask import Flask, render_template, current_app, request, redirect 
+from datetime import datetime, date
+from flask import Flask, render_template, request, redirect , url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -18,8 +18,8 @@ class Post(db.Model):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
-        posts = Post.query.all()
-        return render_template('index.html', posts = posts)
+        posts = Post.query.order_by(Post.due).all()
+        return render_template('index.html', posts = posts, today = date.today())
     else:
         title = request.form.get('title')
         detail = request.form.get('detail')
@@ -37,6 +37,34 @@ def index():
 @app.route('/create')
 def create():
     return render_template('create.html')
+
+@app.route('/detail/<int:id>')
+def read(id):
+    post = Post.query.get(id)
+    return render_template('detail.html', post=post)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+        task = Post.query.get(id)
+
+        db.session.delete(task)
+        db.session.commit()    
+
+        return redirect('/')
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    post = Post.query.get(id)
+    if request.method == 'GET':
+        return render_template('update.html', post=post)
+    else:
+        post.title = request.form.get('title')
+        post.detail = request.form.get('detail')
+        post.due = datetime.strptime(request.form.get('due'), '%Y-%m-%d')
+
+        db.session.commit()
+        return redirect('/')
+
 
 if __name__ == "__main__":
     with app.app_context():
