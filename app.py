0ca397529acbc2f9aa4a76190c3a277f06f80b8f
kohei110 <kohei.nishitani@gmail.com>
2023-03-20 18:01:40 -0700

    flask web app learning

--- app.py
+++ app.py
@@ -1,46 +1,44 @@
-from flask import Flask, render_template, request, redirect, url_for
+# https://tech-diary.net/flask-introduction/
+
+from datetime import datetime
+from flask import Flask, render_template, current_app, request, redirect 
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 db = SQLAlchemy(app)
 
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
 
-    def __repr__(self):
-        return f"{self.id} - {self.title}"
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(30), nullable=False)
+    detail = db.Column(db.String(100))
+    due = db.Column(db.DateTime, nullable=False)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        todo_title = request.form['title']
-        new_todo = Todo(title=todo_title)
-
-        try:
-            db.session.add(new_todo)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return "There was an issue adding your task"
-
+    if request.method == 'GET':
+        posts = Post.query.all()
+        return render_template('index.html', posts = posts)
     else:
-        todos = Todo.query.order_by(Todo.id).all()
-        return render_template('index.html', todos=todos)
+        title = request.form.get('title')
+        detail = request.form.get('detail')
+        due = request.form.get('due')
 
-@app.route('/delete/<int:id>')
-def delete(id):
-    todo_to_delete = Todo.query.get_or_404(id)
+        due = datetime.strptime(due,'%Y-%m-%d')
+        
+        new_post = Post(title = title, detail = detail, due = due)
 
-    try:
-        db.session.delete(todo_to_delete)
+        db.session.add(new_post)
         db.session.commit()
+
         return redirect('/')
-    except:
-        return "There was a problem deleting that task"
+
+@app.route('/create')
+def create():
+    return render_template('create.html')
 
 if __name__ == "__main__":
-    with app.app_context():  # Add this line to set up an application context
-        db.create_all()  # Create the table within the application context
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
